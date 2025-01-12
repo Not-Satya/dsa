939dc02c9fe6d85bfd252e992daf1e46dee05303
Not Satya <satyaiseverywhere@gmail.com>
2025-01-12 12:29:30 +0530

    Update Move_zeroes_to_end.cpp

--- Move_zeroes_to_end.cpp
+++ Move_zeroes_to_end.cpp
@@ -1,4 +1,6 @@
-// Given an arary, move all the zeroes to the end of the array
+// given an array of integers, move all the zeroes to the end of the array
+// and move all the non zero elements to the front maintaining their order
+
 #include <iostream>
 #include <vector>
 using namespace std;
@@ -6,24 +8,27 @@ using namespace std;
 void moveZeroesToEnd(vector<int>& arr) {
     int j = 0, n = arr.size();
 
-    for (int i = 0; i<n; i++) {
-        if (arr[i] == 0) continue;
-        arr[j] = arr[i];
-        j++;
+    for (int i = 0; i < n; i++) {
+        if (arr[i] != 0) {
+            arr[j] = arr[i];
+            j++;
+        }
     }
-    for (j; j<n; j++) arr[j] = 0;
-    return;
+    for (; j < n; j++) arr[j] = 0;
 }
 
 int main() {
     vector<int> arr = {1, 0, 5, 4, 0, 0, 3, 0, 7};
 
-    cout<<"The array after removing all zeroes to the end is: "<<endl;
-    cout<<"[";
-    for (int i=0; i<arr.size(); i++) {
-        cout<<arr[i];
-        if (i < n) cout<<", ";
+    moveZeroesToEnd(arr);
+
+    cout << "The array after moving all zeroes to the end is: " << endl;
+    cout << "[";
+    for (int i = 0; i < arr.size(); i++) {
+        cout << arr[i];
+        if (i < arr.size() - 1) cout << ", ";
     }
-    cout<<"]"<<endl;
+    cout << "]" << endl;
     return 0;
 }
+
